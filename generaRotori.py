6d6a155bc8f8dbba322cc8606d6ac5a3ace0aa51
RobertoGianfelice <rob.gianfelice@gmail.com>
2020-08-18 20:24:44 +0200

    gestione 5 rotori

--- generaRotori.py
+++ generaRotori.py
@@ -10,4 +10,8 @@ random.shuffle(s)
 s2=s[:]
 random.shuffle(s)
 s3=s[:]
-print ("s1=",s1,"\ns2=",s2,"\ns3=",s3)
+random.shuffle(s)
+s4=s[:]
+random.shuffle(s)
+s5=s[:]
+print ("s1=",s1,"\ns2=",s2,"\ns3=",s3,"\ns4=",s4,"\ns5=",s5)
