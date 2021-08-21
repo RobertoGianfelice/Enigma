cb896c314dce5f5728cbc4d3b640169609ae55cf
Roberto Gianfelice <rob.gianfelice@gmail.com>
2021-08-21 14:38:35 +0200

    Update cripta.py
    Aggiunti Commenti

--- cripta.py
+++ cripta.py
@@ -1,3 +1,8 @@
+# Macchina Enigma completa
+# 5 rotori a disposizione costanti
+# 3 rotori attivi nella codifica a sceta dell'utente
+# una pannelo Staker
+
 NUMEROSCAMBI=10
 #scamb=[1,0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 scamb=[]
