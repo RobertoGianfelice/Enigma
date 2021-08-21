8d5170a4b3d2d1572fdeb8378d087d73ec56351f
Roberto Gianfelice <rob.gianfelice@gmail.com>
2021-08-21 14:36:23 +0200

    Update Enigma0.py
    Aggiunto commento

--- Enigma0.py
+++ Enigma0.py
@@ -1,3 +1,5 @@
+# Usando un singolo rotore, costante, acquisisce una frase e la codifica
+
 r1= [3, 24, 0, 6, 7, 4, 12, 17, 11, 23, 21, 15, 18, 19, 16, 14, 1, 20, 8, 13, 5, 9, 25, 2, 10, 22]
 
 parolaInChiaro=input("Inserisci la parola da crittografare (in MAIUSCOLO): ")
